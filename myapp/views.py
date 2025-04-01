5814f25a7011c4938f5fbb03734746a53283b1f9
S41118151 <148747206+S41118151@users.noreply.github.com>
2025-04-01 13:30:24 +0800

    Update views.py

--- myapp/views.py
+++ myapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from datetime import datetime
 # Create your views here.
 def sayhello(request):
-	return HttpResponse("Hello 謝先生")
+	return HttpResponse("41118151 謝日新")
 def hello2(request,username):
 	return HttpResponse("Hello 2 "+username)
 def hello3(request,username):
